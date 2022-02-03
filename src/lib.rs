322529b82fb45b9a69e7a644cc70222fd1745bd1
Soham Sen <soham@aftershoot.com>
2022-02-03 05:29:17 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,15 @@ pub use self::libraw::*;
 #[cfg(windows)]
 #[path = "windows.rs"]
 mod libraw;
-#[cfg(unix)]
+
+#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+#[path = "intel_darwin.rs"]
+mod libraw;
+
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+#[path = "unix.rs"]
+mod libraw;
+
+#[cfg(target_os = "linux")]
 #[path = "unix.rs"]
 mod libraw;
