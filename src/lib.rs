7d7919d1a25128c39e8677edbf95287470a17b27
Uttarayan Mondal <email@uttarayan.me>
2022-04-30 13:20:01 +0530

    Generate bindings on the machine

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
-pub use self::libraw::*;
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(deref_nullptr)]
 
-#[cfg(windows)]
+pub use self::bindings::*;
+
+#[cfg(all(windows, not(feature = "bindgen")))]
 #[path = "windows.rs"]
-mod libraw;
-#[cfg(unix)]
+mod bindings;
+
+#[cfg(all(unix, not(feature = "bindgen")))]
 #[path = "unix.rs"]
-mod libraw;
+mod bindings;
+
+#[cfg(feature = "bindgen")]
+mod bindings;
